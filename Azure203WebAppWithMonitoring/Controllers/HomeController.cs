1db475b3d9cfde39c927898077a6b490f4a2e542
Smruti Ranjan  Panda <smpanda@microsoft.com>
2019-08-11 21:36:11 +0530

    Add project files.

---
+++ Azure203WebAppWithMonitoring/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Azure203WebAppWithMonitoring.Models;
+
+namespace Azure203WebAppWithMonitoring.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Azure203WebAppWithMonitoring/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Azure203WebAppWithMonitoring.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
